e85e1e6715129dfd6b7a05e9727bda303495f64c
Devan Peetz <d.peetz516@gmail.com>
2017-05-04 16:48:38 -0400

    Added IdentityModel Class

---
+++ ElevenNote.Web/Models/IdentityModels.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using SaasEcom.Core.DataServices;
+using SaasEcom.Core.Models;
+using ElevenNote.Models;
+
+namespace MyNotes.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : SaasEcomUser
+    {
+        public virtual ICollection<NoteCreate> Notes { get; set; }
+
+        public virtual BillingAddress BillingAddress { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : SaasEcomDbContext<ApplicationUser>
+    {
+        public DbSet<NoteCreate> Notes { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ApplicationUser>().Map(m => m.MapInheritedProperties());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
